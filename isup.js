d798bc5ca96e635db1b81f78b51039e6591e03c0
thevdude <rb.cubed@gmail.com>
2012-02-29 22:22:15 -0500

    Added isup plugin and last.fm nowplaying plugin

---
+++ isup.js
@@ -0,0 +1,43 @@
+// A little website checking plugin for IRC Node.
+// Author: thevdude (rb.cubed@gmail.com)
+//
+// Hits isup.me to check if a website is down or not.
+
+var http = require('http');
+var irc = global.irc;
+
+var isup_handler = function (act) {
+
+  if (act.params.length === 0) {
+    irc.privmsg(act.source, 'Gimme a URL to check');
+  }
+
+  else {
+    var options = {
+      host: 'www.isup.me',
+      port: 80,
+      path: '/' + act.params
+    };
+
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function(chunk) {
+        if(chunk.indexOf('It\'s just you') != -1){
+          irc.privmsg(act.source, 'It\'s up! http://' + act.params);
+        } else {
+          irc.privmsg(act.source, 'It\'s down!');
+        }
+      });
+    });
+
+    req.on('error', function(e) {
+      console.log('error: ' + e.message);
+    });
+
+    req.end();
+  }
+
+};
+
+exports.name = 'isup';
+exports.handler = isup_handler;
---
+++ lastfm.js
@@ -0,0 +1,53 @@
+// A last.fm nowplaying plugin.
+// Author: thevdude (rb.cubed@gmail.com)
+//
+// npm install lastfm in the plugins folder before using this.
+
+var LastFmNode = require('lastfm').LastFmNode;
+var lastfm = new LastFmNode({
+  api_key: 'ba997d09a17f2dbd74584f0db1f998a9',
+  secret: '8292fd830110c48b8f3654f7c0a83426',
+  useragent: 'thebotdudenp/0.1 NP',
+});
+var irc = global.irc;
+
+var np_handler = function (act) {
+  var lfmuser = '';
+  if (act.params.length === 0) {
+    lfmuser = act.nick;
+  } else {
+    lfmuser = act.params;
+  }
+
+  var request = lastfm.request("user.getRecentTracks", {
+    user: lfmuser,
+    handlers: {
+      success: function(data) {
+        var trck = data.recenttracks.track[0];
+        var msg = '';
+        // Checks if track is Now Playing
+        if('@attr' in trck){
+          if('nowplaying' in trck['@attr'] && trck['@attr'].nowplaying == 'true'){
+            msg = lfmuser + ' is now playing: ' + trck.artist['#text'] + ' - ' + trck.name;
+          } 
+        }
+        //Otherwise, last played
+        else {
+            msg = lfmuser + ' last played: ' + trck.artist['#text'] + ' - ' + trck.name;
+        }
+        //If album name is there, add that in too
+        if(trck.album['#text'] != ''){
+          msg = msg + ' from the album ' + trck.album['#text'];
+        }
+        //Have the bot say it.
+        irc.privmsg(act.source, msg);        
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    }
+  });
+};
+
+exports.name = 'np';
+exports.handler = np_handler;
